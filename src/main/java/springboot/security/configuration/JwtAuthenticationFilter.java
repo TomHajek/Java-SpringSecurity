f4c4dd24e3466c742097c76c9c38954ca182ede1
_hayas <hajcus10@seznam.cz>
2023-10-18 14:08:23 +0200

    [JWT] Checking jwt token and creating jwt service

--- src/main/java/springboot/security/configuration/JwtAuthenticationFilter.java
+++ src/main/java/springboot/security/configuration/JwtAuthenticationFilter.java
@@ -8,6 +8,7 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import springboot.security.service.JwtService;
 
 import java.io.IOException;
 
@@ -21,6 +22,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+
     /**
      *
      * @param request       http request, we can intercept any request and return adequate response
@@ -28,12 +31,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
      * @param filterChain   chain of responsibility design pattern, contains a list of other filters that we need
      *                      so when we call this filter chain .doInternalFilter or .doFilter, it will call next filter
      *                      within the chain
-     *
      */
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        // When we make a call, first we need to extract information from header
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt = authHeader.substring(7);
+        final String userEmail = jwtService.extractUsername(jwt);
+
+        // If token does not meet these conditions, stop here and continue to next filters
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
     }
 
